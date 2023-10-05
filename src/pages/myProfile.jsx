bcff74ad3b182c1d7f3f8c0e5dbe1f8d09ac83d8
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-10-05 16:14:30 +0100

    Update the myprofile component

--- src/pages/myProfile.jsx
+++ src/pages/myProfile.jsx
@@ -4,11 +4,11 @@ import Table from 'react-bootstrap/Table';
 
 const MyProfile = () => {
   const rockets = useSelector((state) => state.rockets);
-  const missions = useSelector((state) => state.missions.missions);
-  const dragons = useSelector((state) => state.dragons);
+  // const missions = useSelector((state) => state.missions.missions);
+  // const dragons = useSelector((state) => state.dragons);
   const reservedRockets = rockets.filter((rocket) => rocket.reserved);
-  const reservedDragons = dragons.filter((dragon) => dragon.reserved);
-  const joinedMissions = missions.filter((mission) => mission.reserved);
+  // const reservedDragons = dragons.filter((dragon) => dragon.reserved);
+  // const joinedMissions = missions.filter((mission) => mission.reserved);
 
   return (
     <div
@@ -20,7 +20,7 @@ const MyProfile = () => {
         flexWrap: 'wrap',
       }}
     >
-      <div style={{ width: '31%', marginTop: '20px' }}>
+      <div style={{ width: '30%', marginTop: '20px' }}>
         <Table striped bordered hover>
           <thead>
             <tr>
@@ -28,15 +28,15 @@ const MyProfile = () => {
             </tr>
           </thead>
           <tbody>
-            {joinedMissions.map((mission) => (
+            {/* {joinedMissions.map((mission) => (
               <tr key={mission.mission_id}>
                 <td>{mission.mission_name}</td>
               </tr>
-            ))}
+            ))} */}
           </tbody>
         </Table>
       </div>
-      <div style={{ width: '31%', marginLeft: '5%', marginTop: '20px' }}>
+      <div style={{ width: '30%', marginLeft: '5%', marginTop: '20px' }}>
         <Table striped bordered hover>
           <thead>
             <tr>
@@ -60,11 +60,11 @@ const MyProfile = () => {
             </tr>
           </thead>
           <tbody>
-            {reservedDragons.map((dragon) => (
+            {/* {reservedDragons.map((dragon) => (
               <tr key={dragon.dragon_id}>
                 <td>{dragon.dragon_name}</td>
               </tr>
-            ))}
+            ))} */}
           </tbody>
         </Table>
       </div>
