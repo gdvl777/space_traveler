79a106c002b95bfb17d819f7260c9a314b9aba77
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-10-05 15:45:07 +0100

    Update naviagtion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-function App() {
-  return (
-    <div className="App" />
-  );
-}
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import './styles/App.css';
+import NavBar from './components/navigation';
+// import Missions from './pages/missions';
+import MyProfile from './pages/myProfile';
+import Rockets from './pages/rockets';
+// import Dragons from './pages/dragon';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => (
+  <Router basename="/EvansSnave">
+    <NavBar />
+    <Routes>
+      <Route path="/space-traveler" element={<Rockets />} />
+      <Route path="/myprofile" element={<MyProfile />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
-import './styles/App.css';
-import NavBar from './components/navigation';
-import Missions from './pages/missions';
-import MyProfile from './pages/myProfile';
-import Rockets from './pages/rockets';
-import Dragons from './pages/dragon';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './redux/store';
 
-function App() {
-  return (
-    <Router basename="/EvansSnave">
-      <NavBar />
-      <Routes>
-        <Route path="/space-traveler" element={<Rockets />} />
-        <Route path="/dragons" element={<Dragons />} />
-        <Route path="/missions" element={<Missions />} />
-        <Route path="/myprofile" element={<MyProfile />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+);
