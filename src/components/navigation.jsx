08a565732dd693b850902b7db7c1f5fcdd516157
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-10-06 21:02:20 +0100

    Add active state to the naviagtion

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -12,16 +12,16 @@ function NavBar() {
         </div>
         <ul className="navlinkcontainer300">
           <li>
-            <NavLink to="/space-traveler" className="navlink300" activeclassname="active-link">Rockets</NavLink>
+            <NavLink to="/space-traveler" className="navlink300">Rockets</NavLink>
           </li>
           <li>
-            <NavLink to="/dragons" className="navlink300" activeclassname="active-link">Dragons</NavLink>
+            <NavLink to="/dragons" className="navlink300">Dragons</NavLink>
           </li>
           <li>
-            <NavLink to="/missions" className="navlink300" activeclassname="active-link">Missions</NavLink>
+            <NavLink to="/missions" className="navlink300">Missions</NavLink>
           </li>
           <li>
-            <NavLink to="/myprofile" className="navlink300" activeclassname="active-link">My Profile</NavLink>
+            <NavLink to="/myprofile" className="navlink300">My Profile</NavLink>
           </li>
         </ul>
       </div>
