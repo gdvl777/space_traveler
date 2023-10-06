232398901e2f3bbc7fc11599d4b05d77bdafc978
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-10-06 16:28:25 +0100

    Revert "Features/mission"

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import missionsReducer from './missions/missionsSlice';
+// import missionsReducer from './missions/missionsSlice';
 import rocketsReducer from './rockets/rocketsSlice';
 import dragonsReducer from './dragons/dragonsSlice';
 
 const rootReducer = {
   rockets: rocketsReducer,
   dragons: dragonsReducer,
-  missions: missionsReducer,
 };
 
 const store = configureStore({
